e765eb81e8595e6c66d949d09cbb59ff4bf63e61
vijay <vijay.jeerla@gmail.com>
2021-09-22 21:14:58 +0530

    test

--- src/LargestString.java
+++ src/LargestString.java
@@ -6,10 +6,8 @@ public class LargestString {
     public static void main(String[] arg) {
 
         List stringList = Arrays.asList("Candidate", null, "BNG", "AAAA", null, "TTTTTTTTTTTTT");
-
         int size = 0;
         int ind = 0;
-
         for (int i = 0; i < stringList.size(); i++) {
             String str = (String) stringList.get(i);
             if(null != str) {

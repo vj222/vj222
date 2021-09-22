0800dca7528b6685f069b677b34503748267635b
vijay <vijay.jeerla@gmail.com>
2021-09-22 21:14:31 +0530

    test

---
+++ src/LargestString.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestString {
+
+    public static void main(String[] arg) {
+
+        List stringList = Arrays.asList("Candidate", null, "BNG", "AAAA", null, "TTTTTTTTTTTTT");
+
+        int size = 0;
+        int ind = 0;
+
+        for (int i = 0; i < stringList.size(); i++) {
+            String str = (String) stringList.get(i);
+            if(null != str) {
+                int len = str.length();
+                if (size < len) {
+                    size = len;
+                    ind = i;
+                }
+            }
+        }
+
+        System.out.println(stringList.get(ind));
+    }
+}
---
+++ src/TestJava.java
@@ -0,0 +1,6 @@
+public class TestJava {
+
+    public static void main(String[] arg){
+        System.out.println("Vijay");
+    }
+}
